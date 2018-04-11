aa589eb15ee302e9905840107e7ddeceeb5e3c0d
mckoyd <dominick.mckoy@gmail.com>
2018-04-11 11:54:45 -0400

    Finished api namespace | tested and works

--- scripts/api.js
+++ scripts/api.js
@@ -3,5 +3,9 @@
 
 // eslint-disable-next-line no-unused-vars
 const api = (() => {
-  
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/domstephanie';
+  const getItems = callback => callback('API module works');
+  return {
+    getItems
+  };
 })();
