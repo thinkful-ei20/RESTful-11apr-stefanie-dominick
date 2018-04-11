d73651379c48b0a5b7b35625619199072d3e90f2
mckoyd <dominick.mckoy@gmail.com>
2018-04-11 12:40:56 -0400

    Tested Add Item event listener | works like a charm

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,4 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
-  api.getItems((items) => {
-    items.forEach((item) => store.addItem(item));
-    shoppingList.render();
-  });
 });
