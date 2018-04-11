3855538a10d9cd5ce932d983ba1864889ae3efcd
mckoyd <dominick.mckoy@gmail.com>
2018-04-11 12:04:38 -0400

    Tested getItems method | works like a charm

--- scripts/api.js
+++ scripts/api.js
@@ -3,8 +3,10 @@
 
 // eslint-disable-next-line no-unused-vars
 const api = (() => {
-  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/domstephanie';
-  const getItems = callback => callback('API module works');
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/domstephanie/items';
+  const getItems = callback => {
+    $.getJSON(BASE_URL, callback);
+  };
   return {
     getItems
   };
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,13 @@
 'use strict';
-/* global shoppingList, store */
+/* global shoppingList, store, api */
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 });
 
-store.items.push(Item.create('apples'));
+api.getItems(function(data) {
+  console.log(data);
+});
+
+console.log(api.BASE_URL);
