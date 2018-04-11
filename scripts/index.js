b5a0cecfd6f60a1633c6e2f0b8db34abf71cd6e0
mckoyd <dominick.mckoy@gmail.com>
2018-04-11 12:24:38 -0400

    Tested getting items and adding them to the store | works like a charm

--- scripts/index.js
+++ scripts/index.js
@@ -4,4 +4,8 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  api.getItems((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
 });
